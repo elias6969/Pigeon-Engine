ece7e8a600df5dce356d8ed482789426665ce9be
elias6969 <eliasanderssons53@gmail.com>
2025-04-04 17:11:10 +0200

    Fixing Structure

---
+++ build/CMakeFiles/PigeonEngine.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lighht19/Documents/Pigeon-Engine/src/BoundingBox.cpp" "CMakeFiles/PigeonEngine.dir/src/BoundingBox.cpp.o" "gcc" "CMakeFiles/PigeonEngine.dir/src/BoundingBox.cpp.o.d"
+  "/home/lighht19/Documents/Pigeon-Engine/src/ModelManager.cpp" "CMakeFiles/PigeonEngine.dir/src/ModelManager.cpp.o" "gcc" "CMakeFiles/PigeonEngine.dir/src/ModelManager.cpp.o.d"
+  "/home/lighht19/Documents/Pigeon-Engine/src/ObjectGenerationIMGUI.cpp" "CMakeFiles/PigeonEngine.dir/src/ObjectGenerationIMGUI.cpp.o" "gcc" "CMakeFiles/PigeonEngine.dir/src/ObjectGenerationIMGUI.cpp.o.d"
+  "/home/lighht19/Documents/Pigeon-Engine/src/ObjectRenderer.cpp" "CMakeFiles/PigeonEngine.dir/src/ObjectRenderer.cpp.o" "gcc" "CMakeFiles/PigeonEngine.dir/src/ObjectRenderer.cpp.o.d"
+  "/home/lighht19/Documents/Pigeon-Engine/src/OpenGLerrorreporting.cpp" "CMakeFiles/PigeonEngine.dir/src/OpenGLerrorreporting.cpp.o" "gcc" "CMakeFiles/PigeonEngine.dir/src/OpenGLerrorreporting.cpp.o.d"
+  "/home/lighht19/Documents/Pigeon-Engine/src/ScreenEffect.cpp" "CMakeFiles/PigeonEngine.dir/src/ScreenEffect.cpp.o" "gcc" "CMakeFiles/PigeonEngine.dir/src/ScreenEffect.cpp.o.d"
+  "/home/lighht19/Documents/Pigeon-Engine/src/filemanager.cpp" "CMakeFiles/PigeonEngine.dir/src/filemanager.cpp.o" "gcc" "CMakeFiles/PigeonEngine.dir/src/filemanager.cpp.o.d"
+  "/home/lighht19/Documents/Pigeon-Engine/src/geoDrawer.cpp" "CMakeFiles/PigeonEngine.dir/src/geoDrawer.cpp.o" "gcc" "CMakeFiles/PigeonEngine.dir/src/geoDrawer.cpp.o.d"
+  "/home/lighht19/Documents/Pigeon-Engine/src/main.cpp" "CMakeFiles/PigeonEngine.dir/src/main.cpp.o" "gcc" "CMakeFiles/PigeonEngine.dir/src/main.cpp.o.d"
+  "" "PigeonEngine" "gcc" "CMakeFiles/PigeonEngine.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
